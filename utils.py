7fb0f33c4a03d76c24deb824796dc17781b05782
Many98 <e.fratrik@gmail.com>
2022-04-23 10:55:39 +0200

    network bit changed and added plotting function

--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ from collections import namedtuple, deque
 from torch.utils.data.dataset import IterableDataset
 import torch.nn as nn
 
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
 
 class DQNetwork(nn.Module):
     """
@@ -14,11 +17,15 @@ class DQNetwork(nn.Module):
     def __init__(self, num_inputs, num_outputs):
         super(DQNetwork, self).__init__()
         self.net = nn.Sequential(
-            nn.Linear(num_inputs, 512),  # `num_inputs` dimensional input state (Input layer)
+            nn.Linear(num_inputs, 64),  # `num_inputs` dimensional input state (Input layer)
             nn.ReLU(),
-            nn.Linear(512, 512),  # Hidden layer
+            nn.Linear(64, 128),  # Hidden layer 1
             nn.ReLU(),
-            nn.Linear(512, num_outputs),  # output is `num_outputs` Q-values for given state corresponding to actions
+            nn.Linear(128, 128),  # Hidden layer 2
+            nn.ReLU(),
+            nn.Linear(128, 64),  # Hidden layer 3
+            nn.ReLU(),
+            nn.Linear(64, num_outputs),  # output is `num_outputs` Q-values for given state corresponding to actions
         )
 
     def forward(self, x):
@@ -45,23 +52,35 @@ class Memory(object):
 
     def __init__(self, capacity, priority_rate=0.0):
         # note that deque will automatically pop old experiences and add new when its full
+        # TODO maybe more optimal solution would be with priority queue which will pop experiences with small priority
         self.memory = deque(maxlen=capacity)
         self.td_errors = deque(maxlen=capacity)
         self.priority_rate = priority_rate  # this is denoted as \alpha in https://arxiv.org/pdf/1511.05952.pdf
 
-    def push(self, state, action, next_state, reward, td_error):
+    def push(self, experience, td_error):
+        """
+
+        :param experience: Experience
+            Named tuple (state, action, next_state, reward)
+        :param td_error: float
+            Temporal difference error
+        :return:
+        """
         """
         Save a experience
         Note that `priority=0` will ensure to have every sample same probability to be sampled i.e. uniform distribution
         """
-        self.memory.append(Experience(state, action, next_state, reward))
+        self.memory.append(experience)
         self.td_errors.append(td_error)
 
-    def sample(self, batch_size):
+    def sample(self, sample_size):
         # probabilities for each samples; if `priority_rate=0` then distribution will be uniform
-        probabilities = np.power(np.array(self.td_errors) + 1e-5, self.priority_rate) / np.sum(self.td_errors)
-        samples = np.random.choice(self.memory, batch_size, replace=False, p=probabilities)
-        states, actions, next_states, rewards = zip(*(sample for sample in samples))
+        priority = np.power(np.array(self.td_errors) + 1e-5, self.priority_rate).astype('float64')
+        probabilities = priority / np.sum(priority)
+        indices = np.random.choice(len(self.memory),
+                                   sample_size if sample_size < len(self.memory) else len(self.memory),
+                                   replace=False, p=probabilities)
+        states, actions, next_states, rewards = zip(*(self.memory[i] for i in indices))
         return (
             np.array(states),
             np.array(actions),
@@ -76,7 +95,8 @@ class Memory(object):
 
 class ReplayDataset(IterableDataset):
     """
-    Iterable Dataset containing the Memory which will be updated with new experiences during training.
+    Iterable Dataset (for streaming data) containing the Memory (stream) which will be updated with new experiences
+    during training.
     """
 
     def __init__(self, memory, sample_size=200):
@@ -92,3 +112,31 @@ class ReplayDataset(IterableDataset):
         states, actions, new_states, rewards = self.memory.sample(self.sample_size)
         for i in range(len(rewards)):
             yield states[i], actions[i], new_states[i], rewards[i]
+
+
+def plot_learning_history(metrics):
+    fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+
+    reward_exploration = [i for i in metrics['exploration']]
+    reward_learning = [i for i in metrics['learning']]
+    #epochs = [i for i in metrics['epoch']]
+    #epochs = [0 for i in range(len(reward_exploration))] + epochs
+    episodes = [i for i in range(len(reward_learning + reward_exploration))]
+
+    ax.plot(episodes[:len(reward_exploration)], reward_exploration, linestyle='dashed')
+    ax.plot(episodes[len(reward_exploration)-1:-1], reward_learning)
+    #ax.xaxis.set_ticks(episodes)
+    #locator = MaxNLocator(nbins=10)
+    #ax.set_xticks
+    #ax.set_xticklabels([(episode, epoch) for episode, epoch in zip(episodes, epochs)])
+    #ax.tick_params(axis='x', labelrotation=45)
+
+    ax.legend(['Initial exploration', 'Learning'], loc='upper right')
+
+    ax.set_title('Agent learning')
+    ax.set_ylabel('Reward')
+    ax.set_xlabel('Episode')
+
+    plt.tight_layout()
+
+    return fig
